1267582a017e975637746a22625a5ef86de11928
johanfal <fredrikalvsaker@gmail.com>
2020-06-07 17:36:29 +0200

    added db and socketio

--- flask-backend/api.py
+++ flask-backend/api.py
@@ -1,8 +1,33 @@
 import time
-from flask import Flask
+from flask import Flask, render_template
 
+from flask_socketio import SocketIO, send, emit
+
+
+from models import *
+
+# Instantiate Flask application
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+# Add secret key
+app.secret_key = 'change this'
+
+
+# Configure database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lxwpcwfzestkpm:45f3f577' \
+                                        '60a95897b082549bb1df75ddcd6de7e662' \
+                                        '87e5a200e66550cfd56aef@ec2-50-17-9' \
+                                        '0-177.compute-1.amazonaws.com:5432' \
+                                        '/d8q777aul5jc81'
+db = SQLAlchemy(app)
+
+
+@app.route('/get_values', methods = ['GET', 'POST'])
+def get_all_values():
+    values = MainEngines.query.all()
+    print(values[0])
+    print(type(values))
+    return {'values': values}
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
