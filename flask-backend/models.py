d97bf8eb85111f949d7ec58c10b12a393e6e8e6a
johanfal <fredrikalvsaker@gmail.com>
2020-06-07 17:35:24 +0200

    added db load

---
+++ flask-backend/models.py
@@ -0,0 +1,54 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class MainEngines(db.Model):
+    """User model."""
+
+    __tablename__ = "MainEngines"
+    time = db.Column(db.DateTime, unique=True, nullable=False)
+    me1_backupbatt = db.Column(db.Float)
+    me1_boostpress = db.Column(db.Float)
+    me1_enginespeed = db.Column(db.Integer)
+    me1_exhausttemp1 = db.Column(db.Float)
+    me1_exhausttemp2 = db.Column(db.Float)
+    me1_fuelrate = db.Column(db.Float)
+    me1_hours = db.Column(db.Integer)
+    me1_lopress = db.Column(db.Float)
+    me1_luboiltemp = db.Column(db.Float)
+    me1_power = db.Column(db.Float)
+    me1_startbatt = db.Column(db.Float)
+    me1_coolanttemp = db.Column(db.Float)
+    me2_backupbatt = db.Column(db.Float)
+    me2_boostpress = db.Column(db.Float)
+    me2_enginespeed = db.Column(db.Integer)
+    me2_exhausttemp1 = db.Column(db.Float)
+    me2_exhausttemp2 = db.Column(db.Float)
+    me2_fuelrate = db.Column(db.Float)
+    me2_hours = db.Column(db.Integer)
+    me2_lopress = db.Column(db.Float)
+    me2_luboiltemp = db.Column(db.Float)
+    me2_power = db.Column(db.Float)
+    me2_startbatt = db.Column(db.Float)
+    me2_coolanttemp = db.Column(db.Float)
+    me3_backupbatt = db.Column(db.Float)
+    me3_boostpress = db.Column(db.Float)
+    me3_enginespeed = db.Column(db.Integer)
+    me3_exhausttemp1 = db.Column(db.Float)
+    me3_exhausttemp2 = db.Column(db.Float)
+    me3_fuelrate = db.Column(db.Float)
+    me3_hours = db.Column(db.Integer)
+    me3_lopress = db.Column(db.Float)
+    me3_luboiltemp = db.Column(db.Float)
+    me3_power = db.Column(db.Float)
+    me3_startbatt = db.Column(db.Float)
+    me3_coolanttemp = db.Column(db.Float)
+    m2insidetemp = db.Column(db.Float)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
