865f108bdeda6fb36a665290a979f82a6c4f67c4
johanfal <fredrikalvsaker@gmail.com>
2020-06-07 19:33:47 +0200

    api call working with id = 1

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,28 +1,16 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
-const App = () => (
-  <Router basename={process.env.PUBLIC_URL}>
-    <div style={{ display: 'flex', flexDirection: 'column','alignItems': 'center','justifyContent': 'center' }}>
-      <Route
-        exact
-        path="/"
-        render={() => (
-          <div>
-            <title>Upload</title>
-            <h2>Upload</h2>
-            <p>
-              Upload your Keras model:
-            </p>
-          </div>
-        )}
-      />
-      {/* <Route path="/upload" component={CubeExample} />
-      <Route path="/asteroids" component={GameExample} />
-      <Route path="/world-map" component={MapExample} /> */}
-    </div>
-  </Router>
-);
+function App() {
+  useEffect(() => {
+    fetch("/get_values").then(response =>
+      response.json().then(data => {
+        console.log(data);
+        })
+    );
+  }, []);
+
+    return <div className="App" />;
+};
 
 export default App;
