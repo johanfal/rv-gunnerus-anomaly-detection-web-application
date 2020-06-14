138babe239a0c73875d389e365d51e8e8b8f1ffb
johanfal <fredrikalvsaker@gmail.com>
2020-06-14 21:28:30 +0200

    styling

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -9,12 +9,12 @@ function App() {
     <div className="header-container">
       <h1>ANOMALY DETECTION</h1>
       <div id="link-container">
-      <h4>
+      <h4 className="repotitle">
         GITHUB REPOSITORIES
       </h4>
       <span>
         <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">[ Web Application ]</a>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">[ Modeling API ]</a>
       </span>
       </div>
