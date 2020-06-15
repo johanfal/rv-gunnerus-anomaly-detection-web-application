9d4f79d9c6c0296cc3010840c04b7a95629279e5
johanfal <fredrikalvsaker@gmail.com>
2020-06-15 11:24:21 +0200

    demonstration purposes

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './styles/main.scss';
 import Selector from './components/Selector'
+import Upload from './components/Upload'
 
 function App() {
   return (
   <div className="content">
     <div id="maincontent">
     <div className="header-container">
+      <div className="upload-container">
+        {/* <Upload /> */}
+      </div>
       <h1>ANOMALY DETECTION</h1>
       <div id="link-container">
       <h4 className="repotitle">
--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import Upload_App from './Upload_App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {/* <Upload_App /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
