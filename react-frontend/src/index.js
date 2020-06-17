d86402b9bd8fe000f1b82e4ca506fc30b242354a
johanfal <fredrikalvsaker@gmail.com>
2020-06-17 15:09:28 +0200

    cleaned up to only contain App

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import Upload_App from './Upload_App';
-
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Upload_App />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
