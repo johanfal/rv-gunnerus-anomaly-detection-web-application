54d97fd7c12db0738988b3e1f87b303283f7e3f3
johanfal <fredrikalvsaker@gmail.com>
2020-06-17 15:11:16 +0200

    styling

--- react-frontend/src/components/Upload.js
+++ react-frontend/src/components/Upload.js
@@ -1,12 +1,15 @@
 import React, {useMemo} from 'react';
 import {useDropzone} from 'react-dropzone';
+import MultiSelect from "@khanacademy/react-multi-select";
 
 const baseStyle = {
-    flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: '10px 20px 10px 20px',
+    display: 'inline-block',
+    width:'250px',
+    textAlign: 'center',
+    padding: '10px 30px 10px 30px',
+    // margin:'20px 68px 20px 68px',
+    // overflow: 'auto',
+    margin: '20px',
     borderWidth: 2,
     borderRadius: 4,
     borderColor: '#eeeeee',
@@ -14,15 +17,15 @@ const baseStyle = {
     outline: 'none',
     transition: 'border .24s ease-in-out',
   };
-  
+
   const activeStyle = {
     borderColor: '#2196f3',
   };
-  
+
   const acceptStyle = {
     borderColor: '#00e676',
-  };
-  
+};
+
   const rejectStyle = {
     borderColor: '#ff1744',
   };
@@ -34,34 +37,26 @@ export const Upload = (props) => {
         isDragActive,
         isDragAccept,
         isDragReject
-      } = useDropzone({accept: 'h5/.h5'});
-    
+      } = useDropzone({accept: props.type});
+
       const style = useMemo(() => ({
         ...baseStyle,
         ...(isDragActive ? activeStyle : {}),
         ...(isDragAccept ? acceptStyle : {}),
-        ...(isDragReject ? acceptStyle : {})
+        ...(isDragReject ? rejectStyle : {})
       }), [
         isDragActive,
         isDragReject,
         isDragAccept
       ]);
       return (
-        <div className="upload">
-          <div {...getRootProps({style})}>
+        <div className="upload-container">
+          <div {...getRootProps({style})} className="upload">
             <input {...getInputProps()} />
             <p>
-                Drag and drop Keras model here, or click to select from files
+                Drag and drop {props.name} here, or click to select from files.
                 <br/><br/>
-                (<em>Keras model format is .h5</em>)
-            </p>
-          </div>
-          <div>
-            <br/>
-            <br/>
-            <br/>
-            <p>Or&nbsp;
-              <a className="sample-link" href="sample_model.h5">use sample model</a>
+                (<em>File format must be {props.format}</em>)
             </p>
           </div>
         </div>
