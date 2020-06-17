199b9b87d965c5a672c8f4fae9d32e6b56f44820
johanfal <fredrikalvsaker@gmail.com>
2020-06-17 15:12:22 +0200

    restructured main app to reduce clutter

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,30 +1,17 @@
 import React from 'react';
 import './styles/main.scss';
-import Selector from './components/Selector'
-import Upload from './components/Upload'
+import ChartDashboard from './components/ChartDashboard';
+import Upload from './components/Upload';
+import Header from './components/Header';
+import Startpage from './components/Startpage';
 
 function App() {
   return (
   <div className="content">
     <div id="maincontent">
-    <div className="header-container">
-      <div className="upload-container">
-        {/* <Upload /> */}
-      </div>
-      <h1>ANOMALY DETECTION</h1>
-      <div id="link-container">
-      <h4 className="repotitle">
-        GITHUB REPOSITORIES
-      </h4>
-      <span>
-        <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">[ Web Application ]</a>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">[ Modeling API ]</a>
-      </span>
-      </div>
-    </div>
-      {/* <Upload /> */}
-        <Selector system='MainEngine1'/>
+      {/* <Header /> */}
+      <Startpage />
+      {/* <ChartDashboard system='MainEngine1'/> */}
     </div>
   </div>
   )
