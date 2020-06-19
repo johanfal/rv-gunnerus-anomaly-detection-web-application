ac7b67a19a9d84a72c51aae363483d7ef1f232ae
johanfal <fredrikalvsaker@gmail.com>
2020-06-19 19:08:11 +0200

    uploading working (also supports using sample files)

--- react-frontend/src/components/Upload.js
+++ react-frontend/src/components/Upload.js
@@ -1,15 +1,13 @@
-import React, { useMemo, useCallback, useState } from 'react';
+import React, { useMemo, useCallback, useState, useEffect, useRef } from 'react';
 import {useDropzone} from 'react-dropzone';
 
 const baseStyle = {
     position: 'relative',
     display: 'inline-block',
-    width:'250px',
+    width:'275px',
     textAlign: 'center',
     verticalAlign: 'middle',
     padding: '10px 30px 10px 30px',
-    // margin:'20px 68px 20px 68px',
-    // overflow: 'auto',
     margin: '20px',
     borderWidth: 2,
     borderRadius: 4,
@@ -19,6 +17,7 @@ const baseStyle = {
     transition: 'border .24s ease-in-out',
   };
 
+  const MAX_FILENAME_LENGTH = 19;
   const activeStyle = {
     borderColor: '#00b3a1',
   };
@@ -29,17 +28,20 @@ export const Upload = (props) => {
   const str_format = props.format.replace(',','/')
   const MAX_SIZE = 31457280 // 30 MB
   const [uploaded, setUploaded] = useState(false);
-  const [file, setFile] = useState(null);
   const [filename, setFilename] = useState(null);
   const [fileProperties, setFileProperties] = useState({});
   const [metadataReceived, setMetadataReceived] = useState(false);
 
+  const resetAll = () => {
+    props.resetProps(props.id);
+    setUploaded(false);
+    setFilename(null);
+    setFileProperties({});
+    setMetadataReceived(false);
+  }
 
   const onDrop = useCallback((acceptedFiles, fileRejections) => {
-    setUploaded(null);
-    setFile(null);
-    setFilename(null);
-
+    resetAll();
     if(acceptedFiles.length > 1){
       alert(`You attempted to upload more than one '${str_format}' file.`)
     }
@@ -64,69 +66,115 @@ export const Upload = (props) => {
       const fileExtension = re.exec(name)[0]
       data.append('file', acceptedFiles[0]);
       data.append('filename', name);
-
+      console.log(data)
       setUploaded(true);
-      setFile(acceptedFiles[0])
+
+      props.sendFilename(name, props.id) // send filename and type to parent
 
       if(props.id === 'keras-model'){
+      // if(props.id === 'keras-model' && !props.useSampleFiles){
         onModelUpload(data);
       }
+      // if(props.id === 'scaler' && !props.useSampleFiles){
       if(props.id === 'scaler'){
         onScalerUpload(data);
       }
 
       const nl = name.length;
       const fl = fileExtension.length;
-      if(nl > 30 + fl){
+      if(nl > MAX_FILENAME_LENGTH + fl){
           name = name.substring(0,15) + '...' +
-                  name.substring(nl-fl-2, nl-fl) + fileExtension
+                  name.substring(nl-fl-3, nl-fl) + fileExtension
         }
         setFilename(name);
       }
-      console.log(acceptedFiles)
-      console.log(fileRejections)
-
-    })
-
+    });
 
     const onModelUpload = (data) => {
         fetch(
-          'keras_model', {
+          `keras_model/${false}`, {
           method: 'POST',
           body: data
         }).then((response) => {
           response.json().then((data) => {
-            setFileProperties({
-              inp: data.fileprops.inp,
-              out: data.fileprops.out,
-              timesteps: data.fileprops.timesteps
-          });
+            if(data.fileprops !== false){
+              const modelProperties = {
+                inp: data.fileprops.inp,
+                out: data.fileprops.out,
+                timesteps: data.fileprops.timesteps
+              }
+              setFileProperties(modelProperties);
+              props.sendModelProperties(modelProperties);
+            } else{
+              setFileProperties(false);
+              props.sendFilename(null, props.id);
+              props.sendModelProperties({input:null,output:null,timesteps:null})
+            }
 
           setMetadataReceived(true);
 
           })
-        })
+        });
     }
 
     const onScalerUpload = (data) => {
-      console.log('Will handle this scaler bad boy!')
       fetch(
-        'scaler', {
+        `scaler/${false}`, {
         method: 'POST',
         body: data
       }).then((response) => {
         response.json().then((data) => {
-          setFileProperties({
-            type: data.fileprops.type,
-            features: data.fileprops.features,
-            samples: data.fileprops.samples
-        });
+          if(data.fileprops !== false){
+            setFileProperties({
+              type: data.fileprops.type,
+              features: data.fileprops.features,
+              samples: data.fileprops.samples
+          });
+          } else{
+            setFileProperties(false);
+            props.sendFilename(null, props.id);
+          }
 
-        setMetadataReceived(true);
-
+          setMetadataReceived(true);
         })
-      })
+      });
     }
+
+
+    useEffect(() => {
+      if(props.useSampleFiles){
+        setUploaded(true);
+        if(props.id === 'keras-model'){
+          setFilename('sample_model.h5');
+          fetch(`keras_model/${true}`).then((response) => {
+            response.json().then((data) => {
+              const modelProperties = {
+                inp: data.fileprops.inp,
+                out: data.fileprops.out,
+                timesteps: data.fileprops.timesteps
+              }
+              setFileProperties(modelProperties);
+              props.sendModelProperties(modelProperties);
+            setMetadataReceived(true);
+            })
+          });
+        }
+        if(props.id === 'scaler'){
+          setFilename('sample_scaler.pckl');
+          fetch(`scaler/${true}`).then((response) => {
+            response.json().then((data) => {
+              console.log(data.fileprops)
+              setFileProperties({
+                type: data.fileprops.type,
+                features: data.fileprops.features,
+                samples: data.fileprops.samples
+            });
+            setMetadataReceived(true);
+            })
+          });
+        }
+        }
+      }, [props.useSampleFiles]);
 
     const {
         getRootProps,
@@ -148,11 +196,22 @@ export const Upload = (props) => {
 
 
       const getFileSummary = (id) => {
+
+        if(!fileProperties){
+          return <div id="fileprops-error" style={{color: 'red'}}>
+          <br/>
+          <em>
+            Could not read {props.name} properties..
+          </em>
+          </div>
+        }
+
         if(id === 'keras-model'){
           return !metadataReceived ? <em>
+            <br/>
             Reading model parameters..
             </em> : <div id="model-upload-properties">
-            <em>Specifications found in uploaded model:</em><br/>
+            <em>Specifications found for uploaded model:</em><br/>
               Number of input columns: {fileProperties.inp} <br/>
               Number of predicted output columns: {fileProperties.out} <br/>
               Timesteps used per prediction: {fileProperties.timesteps} <br/>
@@ -160,12 +219,13 @@ export const Upload = (props) => {
         }
         if(id === 'scaler'){
           return !metadataReceived ? <em>
+          <br/>
             Reading scaler parameters..
             </em> : <div id="model-upload-properties">
-            <em>Specifications found in uploaded scaler:</em><br/>
+            <em>Specifications found for uploaded scaler:</em><br/>
               Scaler type: {fileProperties.type} <br/>
               Number of columns scaled: {fileProperties.features} <br/>
-              Samples during creation: {fileProperties.samples} <br/>
+              Samples experienced by scaler: {fileProperties.samples} <br/>
             </div>
         }
       }
@@ -176,16 +236,23 @@ export const Upload = (props) => {
             <input {...getInputProps()} />
               {
               !uploaded ?
-                [<p>
-                  Drag and drop {props.name} here, or click to select from files.
-                  <br/><br/>
+                [<div>
+                <br/>
+                  Drag and drop {props.name} here,<br/>or click to select from files.
+                  <br/><br/><br/>
                   (<em>File format must be {str_format}</em>)
-                </p>] :
-                [<p>
-                  '{filename}' succesfully uploaded.
-                  <br/><br/>
+                </div>] :
+                [<div>
+                <br/>
+                  {props.name.charAt(0).toUpperCase() + props.name.slice(1)}
+                  <div style={{color: 'rgb(71, 255, 71)'}}>
+                    '{filename}' uploaded.
+                  </div>
+                  <br/>
+                  <div>
                     {getFileSummary(props.id)}
-                </p>
+                  </div>
+                </div>
                 ]
               }
           </div>
