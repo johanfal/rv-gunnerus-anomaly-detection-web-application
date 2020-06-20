cd697167afefc330241708505e2a3043014dbf74
johanfal <fredrikalvsaker@gmail.com>
2020-06-21 00:31:11 +0200

    formatting, introduced props, changed signal selection, added model fetch and instantiation, added some charting functionality (pred/no pred)

--- react-frontend/src/components/ChartDashboard.js
+++ react-frontend/src/components/ChartDashboard.js
@@ -12,24 +12,31 @@ export class ChartDashboard extends React.Component {
     this.state = {
       selected: [], // signals
       options: [], // multi-select options
-      chart_items: {}, // rendered charts
-      sio_status: false, // socket status
+      chartItems: {}, // rendered charts
+      sioStatus: false, // socket status
+      allSignals: [],
     };
+    this.system = this.props.system;
+    this.inputs = this.props.inputs;
+    this.outputs = this.props.outputs;
+    this.timesteps = this.props.modelTimesteps;
   }
   socket;
-  system = this.props.selectedSystem;
-  inputs = this.props.selectedInputs;
-  outputs = this.props.selectedOutputs;
   // If component is succesfully mounted
   componentDidMount() {
-    this.on_reload(); // call reload function
-    this.signals(); // get list of signals based on API call to database
+    fetch("keras_model/true");
+    fetch("scaler/true");
+    const strInputs = this.inputs.join(",");
+    const strOutputs = this.outputs.join(",");
+    fetch(`instantiate_thread/${this.system}/${strInputs}/${strOutputs}`);
+    this.onReload(); // call reload function
+    this.setSignalSelection(); // get list of signals based on API call to database
   }
 
   getStatus = (obj) => Object.keys(obj).length > 0;
 
   // Restart API thread if the page is reloaded
-  on_reload = () => {
+  onReload = () => {
     if (window.performance) {
       if (performance.navigation.type === 1) {
         fetch("reload"); // restart thread through API call
@@ -37,138 +44,155 @@ export class ChartDashboard extends React.Component {
     }
   };
 
-  // Get list of signals
-  signals = () => {
-    let chart_items = this.state.chart_items;
-
-    // API get-request
-    fetch("signals").then((response) =>
-      response.json().then((data) => {
-        this.signals = data.signals;
-        this.options = []; // Is this needed?
-
-        // Filter out 'id' and 'time' from list of possible signals
-        this.signals = this.signals.filter((e) => e !== "id" && e !== "time");
-
-        // Loop through signals
-        for (let i = 0; i < this.signals.length; i++) {
-          // Add signal as selector option with label and value
-          this.options.push({
-            label: this.signals[i],
-            value: this.signals[i],
-          });
-        }
-
-        // Update mounted states
-        this.setState({
-          options: this.options,
-          chart_items: chart_items,
-          sio_status: this.getStatus(chart_items),
-        });
-      })
-    );
+  setSignalSelection = () => {
+    const options = this.state.options;
+    const outputs = this.outputs;
+    const allSignals = [...new Set([...this.inputs, ...this.outputs])];
+    for (let signal of allSignals) {
+      options.push({
+        label: outputs.includes(signal)
+          ? `${signal} (with prediction)`
+          : signal,
+        value: signal,
+      });
+    }
+    this.setState({
+      options: options,
+      allSignals: allSignals,
+    });
   };
+
+  // Get list of signals
+  // signals = () => {
+  //   let chartItems = this.state.chartItems;
+
+  //   // API get-request
+  //   fetch(`signals/${system}`).then((response) =>
+  //     response.json().then((data) => {
+  //       this.signals = data.signals;
+
+  //       // Filter out 'id' and 'time' from list of possible signals
+  //       this.signals = this.signals.filter((e) => e !== "id" && e !== "time");
+
+  //       // Loop through signals
+  //       for (let i = 0; i < this.signals.length; i++) {
+  //         // Add signal as selector option with label and value
+  //         this.options.push({
+  //           label: this.signals[i],
+  //           value: this.signals[i],
+  //         });
+  //       }
+
+  //       // Update mounted states
+  //       this.setState({
+  //         options: this.options,
+  //         chartItems: chartItems,
+  //         sioStatus: this.getStatus(chartItems),
+  //       });
+  //     })
+  //   );
+  // };
 
   connect = () => {
     this.socket = io.connect(`/?system=${this.system}`);
   };
 
-  componentDidUpdate(_prev_props, prev_state) {
-    let update_state = false; // affects useState at end of lifecycle method
+  componentDidUpdate(_prevProps, prevState) {
+    let updateState = false; // affects useState at end of lifecycle method
     const selected = this.state.selected;
-    const sio_status = this.state.sio_status;
-    let chart_items = this.state.chart_items;
-    const added = selected.filter((sig) => !prev_state.selected.includes(sig));
-    const deleted = prev_state.selected.filter(
-      (sig) => !selected.includes(sig)
-    );
+    const sioStatus = this.state.sioStatus;
+    let chartItems = this.state.chartItems;
+    const added = selected.filter((sig) => !prevState.selected.includes(sig));
+    const deleted = prevState.selected.filter((sig) => !selected.includes(sig));
 
-    this.manage_io_connection(sio_status, prev_state.sio_status);
+    this.manageSocketConnection(sioStatus, prevState.sioStatus);
 
     // If a signal is selected
     if (added.length > 0) {
-      update_state = true;
-      chart_items = this.add_new_charts(added, chart_items);
+      updateState = true;
+      chartItems = this.addNewCharts(added, chartItems);
     }
 
     // If a signal is deselected
     if (deleted.length > 0) {
-      update_state = true;
-      chart_items = this.delete_unselected_charts(deleted, chart_items);
+      updateState = true;
+      chartItems = this.deleteUnselectedCharts(deleted, chartItems);
     }
 
     // Update socketIO connection or disconnection
-    if (sio_status) {
-      if (added.length > 0 || deleted.length > 0 || prev_state.modified) {
+    if (sioStatus) {
+      if (added.length > 0 || deleted.length > 0 || prevState.modified) {
         this.socket.disconnect();
         this.connect();
       }
     }
 
     // If socketIO is connected, get values
-    if (sio_status) {
-      this.get_values(selected);
+    if (sioStatus) {
+      this.getValues(selected);
     }
 
-    if (update_state) {
-      this.set_updated_state(chart_items);
+    if (updateState) {
+      this.setUpdatedState(chartItems);
     }
   }
 
-  manage_io_connection = (current_status, previous_status) => {
-    if (current_status !== previous_status) {
-      current_status ? this.connect() : this.socket.disconnect();
+  manageSocketConnection = (currentStatus, previousStatus) => {
+    if (currentStatus !== previousStatus) {
+      currentStatus ? this.connect() : this.socket.disconnect();
     }
   };
 
-  add_new_charts = (newly_selected, selected_items) => {
-    for (let sig of newly_selected) {
-      selected_items[sig] = this.add_chart(sig, sig, {
+  addNewCharts = (newlySelected, selectedItems) => {
+    for (let sig of newlySelected) {
+      const isSignalToPredict = this.outputs.includes(sig); // boolean
+      selectedItems[sig] = this.addChart(sig, sig, isSignalToPredict, {
         id: undefined,
         time: undefined,
         signal: undefined,
       });
     }
-    return selected_items;
+    return selectedItems;
   };
 
-  delete_unselected_charts = (newly_deselected, selected_items) => {
-    for (let sig of newly_deselected) {
-      delete selected_items[sig];
+  deleteUnselectedCharts = (newlyDeselected, selectedItems) => {
+    for (let sig of newlyDeselected) {
+      delete selectedItems[sig];
     }
-    return selected_items;
+    return selectedItems;
   };
 
-  get_values = (selected) => {
+  getValues = (selected) => {
     if (selected.length > 0) {
       fetch(`/update_selected/${selected.join()}`);
-      this.socket.on("values", this.store_reading);
+      this.socket.on("values", (values) => this.storeReading(values));
     }
   };
 
-  store_reading = (values) => {
-    var chart_items = {};
+  storeReading = (values) => {
+    var chartItems = {};
     const selected = this.state.selected;
     const time = values.time;
     const id = values.id;
     for (let sig of selected) {
-      chart_items[sig] = this.add_chart(sig, sig, {
+      const isSignalToPredict = this.outputs.includes(sig); // boolean
+      chartItems[sig] = this.addChart(sig, sig, isSignalToPredict, {
         id: id,
         time: time,
         signal: values[sig],
       });
     }
-    this.setState({ chart_items: chart_items });
+    this.setState({ chartItems: chartItems });
   };
 
-  add_chart = (sensor, key, values) => {
-    return <Chart sensor_id={sensor} key={key} values={values} pred={true} />;
+  addChart = (sensor, key, pred, values) => {
+    return <Chart sensorId={sensor} key={key} values={values} pred={pred} />;
   };
 
-  set_updated_state = (selected_items) => {
+  setUpdatedState = (selectedItems) => {
     this.setState({
-      chart_items: selected_items,
-      sio_status: this.getStatus(selected_items),
+      chartItems: selectedItems,
+      sioStatus: this.getStatus(selectedItems),
       modified: true,
     });
   };
@@ -176,8 +200,8 @@ export class ChartDashboard extends React.Component {
   render() {
     const selected = this.state.selected;
     const options = this.state.options;
-    const chart_items = this.state.chart_items;
-    const no_signals = Object.keys(this.signals).length === 0;
+    const chartItems = this.state.chartItems;
+    const noSignals = Object.keys(this.state.allSignals).length === 0;
     return (
       <div className="selector-chart-container">
         <div className="selector-container">
@@ -187,17 +211,17 @@ export class ChartDashboard extends React.Component {
             selected={selected}
             onSelectedChanged={(selected) => this.setState({ selected })}
             overrideStrings={{
-              selectSomeItems: no_signals
+              selectSomeItems: noSignals
                 ? "No signals found.."
-                : "Select signals..",
-              allItemsAreSelected: "All signals selected",
+                : "Select signals",
+              allItemsAreSelected: "Showing all signals",
               selectAll: "Select all",
             }}
             disableSearch={true}
-            isLoading={no_signals ? true : false}
+            isLoading={noSignals ? true : false}
           />
         </div>
-        <div className="charts-container">{Object.values(chart_items)}</div>
+        <div className="charts-container">{Object.values(chartItems)}</div>
       </div>
     );
   }
