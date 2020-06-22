96b4d156c4171224d95708f2019f3d660646a52d
johanfal <fredrikalvsaker@gmail.com>
2020-06-22 17:15:40 +0200

    formatting

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,20 +1,17 @@
-import React from 'react';
-import './styles/main.scss';
-import ChartDashboard from './components/ChartDashboard';
-import Upload from './components/Upload';
-import Header from './components/Header';
-import Startpage from './components/Startpage';
+import React from "react";
+import "./styles/main.scss";
+import Header from "./components/Header";
+import Startpage from "./components/Startpage";
 
 function App() {
   return (
-  <div className="content">
-    <div id="maincontent">
-      <Header />
-      <Startpage />
-      {/* <ChartDashboard system='MainEngine1'/> */}
+    <div className="content">
+      <div id="maincontent">
+        <Header />
+        <Startpage />
+      </div>
     </div>
-  </div>
-  )
-};
+  );
+}
 
 export default App;
