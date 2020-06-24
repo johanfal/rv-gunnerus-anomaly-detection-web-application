6330e81379f6a283b7efe572e66177a8ac647678
johanfal <fredrikalvsaker@gmail.com>
2020-06-24 13:38:29 +0200

    implementation with about page

--- react-frontend/src/components/Header.js
+++ react-frontend/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const Header = () => {
+export const Header = (props) => {
   const onHomeClick = () => {
     window.location.reload(true);
   };
@@ -8,31 +8,41 @@ export const Header = () => {
     <div className="header-container">
       <div className="homebtn-container">
         <button className="homebtn" onClick={onHomeClick}>
-          <i className="home-icon" className="fa fa-home" />
+          <i className="fa fa-home" />
         </button>
       </div>
-      <h1>ANOMALY DETECTION</h1>
-      <div id="link-container">
-        <h4 className="repotitle">GITHUB REPOSITORIES</h4>
-        <span>
-          <a
-            href="https://www.google.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            [ Web Application ]
-          </a>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <a
-            href="https://www.google.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            [ Modeling API ]
-          </a>
-        </span>
-      </div>
+      <h1>
+        {props.about
+          ? "ABOUT"
+          : `ANOMALY DETECTION ${
+              props.dashboard ? "DASHBOARD" : "STARTPAGE"
+            }`}
+      </h1>
+      {props.about
+        ? [
+            <div id="link-container">
+              <h4 className="repotitle">GITHUB REPOSITORIES</h4>
+              <span>
+                <a
+                  href="https://www.google.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  [ Web Application ]
+                </a>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a
+                  href="https://www.google.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  [ Modeling API ]
+                </a>
+              </span>
+            </div>,
+          ]
+        : null}
     </div>
   );
 };
