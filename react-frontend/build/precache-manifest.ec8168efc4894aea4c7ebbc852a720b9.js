dda0a3714ae45ee4c8f65a43ff0c1f4dbd7c8d06
johanfal <fredrikalvsaker@gmail.com>
2020-06-26 00:00:43 +0200

    production build update - socket connection

---
+++ react-frontend/build/precache-manifest.ec8168efc4894aea4c7ebbc852a720b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e7012c123dd25e6a5527aff67e030e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b93404746e537c9a7ee",
+    "url": "/static/css/main.72085f92.chunk.css"
+  },
+  {
+    "revision": "fa68678a739de6a9211b",
+    "url": "/static/js/2.fcdf1771.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.fcdf1771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b93404746e537c9a7ee",
+    "url": "/static/js/main.4d0391e8.chunk.js"
+  },
+  {
+    "revision": "ac835f9e726288460dc6",
+    "url": "/static/js/runtime-main.20b9e64d.js"
+  }
+]);
